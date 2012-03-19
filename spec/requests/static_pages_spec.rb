425e2c7c0e429e832e27c6a4db8246fb3b4b9cd3
Richard King <rkingucla@ymail.com>
2012-03-19 11:34:59 -0700

    add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
@@ -12,9 +15,27 @@ describe "Static pages" do
     it "should have the content 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => ' | Home')
-    end    
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+        
   end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => ' | Contact')
+    end
+  end 
   
   describe "Help page" do
     
